5fa86493a3160f4c6e4593f36328f28598e6a0b3
Aaron Pearson <pearsonaaron.56@gmail.com>
2021-11-11 00:52:37 -0500

    3.2.8

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,10 +75,21 @@ var fight = function(enemyName) {
 // repeat and execute as long as the enemy-robot is alive
 }
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round" + ( i + 1) );
+    }
+    
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+
+    // use a debugger to pause script from running and check what's going on at that moment in the code
 
     // call fight function with enemy-robot
-    fight(pickedEnemyName);
-}
+        fight(pickedEnemyName);
+    }
+   //else {
+    //window.alert("You have lost your robot in battle! Game Over!");
+    //break;
+
+
 
