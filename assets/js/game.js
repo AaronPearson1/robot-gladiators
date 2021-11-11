b670101ff63f114471e74a03823c2739ed8be132
Aaron Pearson <pearsonaaron.56@gmail.com>
2021-11-10 22:28:24 -0500

    Function Arguments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,10 @@
+// What follows after this is called pseudocode. Pseudocode is a plain-language of the steps that an algorithm or applcation must complete.
+
+// Game States
+// "Win" - PLayer robot has defated all enemy-robots
+//     * Fight all enemy-robots
+//     * Defeat each enemy-robot
+// "Lose" - Player robot's health is zero or less
 var playerName = window.prompt("what is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,11 +13,11 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -58,12 +65,14 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
         }
         // if no (false), ask question again by running fight() again
         else {
-            fight()
+            fight();
         }
-        window.alert(playerName + " has chosen to skip the fight!");
+        // if player did not chose 1 or 2 in prompt
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }   
 }
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+//fight();
